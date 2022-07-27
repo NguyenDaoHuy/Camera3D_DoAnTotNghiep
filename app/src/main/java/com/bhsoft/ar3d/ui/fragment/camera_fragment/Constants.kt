6934e94d4eff50dd8459823f16e496f81a715c67
HauTV <hautv.bhsoft@gmail.com>
2022-07-27 17:54:09 +0700

    update save photo to DCIM/ar3d

--- app/src/main/java/com/bhsoft/ar3d/ui/fragment/camera_fragment/Constants.kt
+++ app/src/main/java/com/bhsoft/ar3d/ui/fragment/camera_fragment/Constants.kt
@@ -4,6 +4,5 @@ object Constants {
     const val TAG = "cameraX"
     const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
     const val REQUEST_CODE_PERMISSIONS = 123
-    const val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA,android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
 }
